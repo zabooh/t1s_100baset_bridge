6b94d0c341b07e462bd80ff44802907c103d838e
zabooh <martin_ruppert@gmx.de>
2026-04-01 15:21:02 +0200

    resuduals not to forget

--- firmware/T1S_100BaseT_Bridge.X/flash_all.py
+++ firmware/T1S_100BaseT_Bridge.X/flash_all.py
@@ -10,6 +10,7 @@ Aufruf:
 """
 import sys
 import os
+import argparse
 
 # --- Konfiguration -----------------------------------------------------------
 _HERE = os.path.dirname(os.path.abspath(__file__))
@@ -24,16 +25,21 @@ sys.path.insert(0, _HERE)
 from mdb_flash import flash
 
 def main():
+    ap = argparse.ArgumentParser(description="Flash both boards via MDB")
+    ap.add_argument("--swd-khz", type=int, default=2000,
+                    help="Requested SWD clock in kHz (best effort, default: 2000)")
+    args = ap.parse_args()
+
     errors = 0
 
     print("\n### Flash FOLLOWER ###")
-    rc = flash(HEX_FILE, FOLLOWER_SERIAL, label="FOLLOWER")
+    rc = flash(HEX_FILE, FOLLOWER_SERIAL, label="FOLLOWER", swd_khz=args.swd_khz)
     if rc != 0:
         print("[FOLLOWER] FEHLER beim Programmieren!")
         errors += 1
 
     print("\n### Flash GRANDMASTER ###")
-    rc = flash(HEX_FILE, GRANDMASTER_SERIAL, label="GRANDMASTER")
+    rc = flash(HEX_FILE, GRANDMASTER_SERIAL, label="GRANDMASTER", swd_khz=args.swd_khz)
     if rc != 0:
         print("[GRANDMASTER] FEHLER beim Programmieren!")
         errors += 1
